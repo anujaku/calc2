bae93d34517d973cd26c5538e7fd03bbd7e2fc99
anujaku <avk5@njit.edu>
2021-11-21 16:10:17 -0500

    Resolved pylint errors

--- calc/calculations/division.py
+++ calc/calculations/division.py
@@ -2,10 +2,10 @@
 from calc.calculations.calculation import Calculation
 
 class Division(Calculation):
-     """Division calculation object"""
-     def get_result(self):
-         """get the division results"""
-         result = 1.0
-         for value in self.values:
-             result = result / value
-         return result
+    """Division calculation object"""
+    def get_result(self):
+        """get the division results"""
+        result = 1.0
+        for value in self.values:
+            result = result / value
+        return result
--- calc/calculations/subtraction.py
+++ calc/calculations/subtraction.py
@@ -4,11 +4,11 @@ import pprint
 from calc.calculations.calculation import Calculation
 
 class Subtraction(Calculation):
-     """subtraction calculation object"""
-     def get_result(self):
-         """get the subtraction results"""
-         difference_of_values = 0.0
-         for value in self.values:
-             difference_of_values =   difference_of_values - value
-             pprint.pprint(value)
-         return difference_of_values
+    """subtraction calculation object"""
+    def get_result(self):
+        """get the subtraction results"""
+        difference_of_values = 0.0
+        for value in self.values:
+            difference_of_values =   difference_of_values - value
+            pprint.pprint(value)
+        return difference_of_values
